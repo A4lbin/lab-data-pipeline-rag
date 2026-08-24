b12d5ad0b904bf301e575af03466a2caf679c821
Albin Abraham <gettoalbin@gmail.com>
2026-08-25 03:08:26 +0530

    added clean lab data to sqlite3 database and made seed.py to connect

---
+++ database/seed.py
@@ -0,0 +1,15 @@
+import sqlite3
+import pandas as pd
+
+CSV_FILE="../data/processed/lab_data_clean.csv"
+DATABASE="lab.db"
+
+df= pd.read_csv(CSV_FILE)
+
+connection = sqlite3.connect(DATABASE)
+
+df.to_sql("records",connection,if_exists="replace",index=False)
+
+connection.close()
+
+print("Database created successfully :)")
--- src/etl.py
+++ src/etl.py
@@ -3,7 +3,6 @@ import pandas as pd
 RAW_FILE = "../data/raw/4x4x4_SI.csv"
 PROCESSED_FILE = "../data/processed/lab_data_clean.csv"
 
-dff={}
 def extract():
     return pd.read_csv(RAW_FILE)
 
@@ -26,14 +25,24 @@ def transform(df):
         df[column] = df[column].str.strip()
     return df
 
+def validate(df):
+
+    if df["uid"].isna().any():
+        raise ValueError("UID contains missing values")
+
+    if not df["uid"].is_unique:
+        raise ValueError("UID is not unique")
+
+    return df
+
 def run_pipeline():
     df=extract()
     df=transform(df)
-    dff=df.copy()
+    validate(df)
     load(df)
-    return dff
+    return df
 
 dff=run_pipeline()
 # print(dff)
-if(dff["uid"].is_unique==1):
-    print("uid is unique")
+# if(dff["uid"].is_unique==1):
+#     print("uid is unique")
