b05220c15a346578aff61e0579b3d605ade1330e
Albin Abraham <gettoalbin@gmail.com>
2026-08-26 04:14:18 +0530

    changed test_search.py and fixed how queries were printed and also tested retrieval quality and found issue so uid:uid,text:text is not a good form for embedding

--- src/test_search.py
+++ src/test_search.py
@@ -18,9 +18,33 @@ embedded_documents = get_embeddings(documents)
 
 
 queries = [
+    # Peptide identity
     "experiments involving peptide PZ2",
+    "experiments involving peptide MZ2R",
+    "experiments involving peptide Z2",
+    "experiments involving peptide AG3",
     "experiments involving peptide Z2M6I",
-    "experiments involving peptide MZ2R"
+    "experiments involving peptide MZ2",
+
+    # # Exact numerical conditions
+    # "experiments with peptide concentration 0.0002",
+    # "experiments with peptide concentration 0.000065",
+    # "experiments with HAuCl4 concentration 0.0002",
+    # "experiments with HAuCl4 concentration 0.000093",
+    # "experiments with HEPES concentration 0.001",
+    # "experiments with HEPES concentration 0.000055",
+
+    # # Combination of categorical + numerical
+    # "PZ2 experiments with HAuCl4 concentration 0.0002",
+    # "MZ2R experiments with HEPES concentration 0.000055",
+    # "Z2 experiments with peptide concentration 0.0002",
+    # "AG3 experiments with HAuCl4 concentration 0.000040",
+
+    # # Location/sample queries
+    # "experiments in slot 4",
+    # "experiments in slot 5",
+    # "experiment at well index 31",
+    # "experiment at well index 63",
 ]
 for query in queries:
 
@@ -33,12 +57,13 @@ for query in queries:
         embedded_documents,
         top_k=5
     )
-for rank, result in enumerate(results, start=1):
+    for rank, result in enumerate(results, start=1):
 
         print(f"\nRank: {rank}")
         print("UID:", result["uid"])
         print("Score:", round(result["score"], 4))
         print(result["text"])
+
 # for result in results:
 
 #     print("UID:", result["uid"])
